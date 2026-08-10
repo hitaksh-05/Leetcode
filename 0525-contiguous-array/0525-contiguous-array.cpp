cd6292f12e2233b3d3712fbe24f5338b0667a1e1
Hitaksh Thaker <hitaksh.t@ahduni.edu.in>
2026-08-10 23:50:55 +0530

    Time: 21 ms (74.96%), Space: 88.2 MB (93.53%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        
+        int n= nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                nums[i]=-1;
+            }
+        }
+        
+        int sum=0;
+        int maxlength=0;
+        
+        unordered_map<int,int>mp;
+        mp[0]=-1;
+        
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            
+            if(mp.find(sum)!=mp.end()){
+                
+                int last= mp[sum];
+                maxlength= max(maxlength, i-last);
+            }
+            else{
+                mp[sum]=i;
+            }
+        }
+        return maxlength;
+    }
+};
