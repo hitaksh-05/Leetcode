352f70cd3a40ff364146ec24edd9cea32ef5a179
Hitaksh Thaker <hitaksh.t@ahduni.edu.in>
2026-08-11 23:36:37 +0530

    Time: 31 ms (20.23%), Space: 88.4 MB (43.59%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -5,7 +5,7 @@ public:
         int n= nums.size();
         for(int i=0;i<n;i++){
             if(nums[i]==0){
-                nums[i]=-1;
+                nums[i]= nums[i]-1;
             }
         }
         
