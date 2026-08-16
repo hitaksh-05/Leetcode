4310513f3f0c626474f413163813062913af032b
Hitaksh Thaker <hitaksh.t@ahduni.edu.in>
2026-08-16 20:53:29 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.6%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int  start=0, end=x;
+        long long mid;
+        
+        while(start<=end){
+            mid= start+(end-start)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else if(mid*mid<x){
+                start= mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return end;
+    }
+};
